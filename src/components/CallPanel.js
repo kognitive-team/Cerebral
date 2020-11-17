2a5192d5cfbd3cfed0246b771176c04a7bb3bf90
SaiThotapalli99 <Sai.Thotapalli@bekognitive.com>
2020-11-16 22:19:37 -0500

    Test

--- src/components/CallPanel.js
+++ src/components/CallPanel.js
@@ -49,52 +49,32 @@ const useStyles = makeStyles((theme) => ({
   
 
 const CallPanel = () => {
-  const classes = useStyles();
-  const [available, setAvailable] = useState(false);
-
- 
+  const classes = useStyles()
+  const [available, setAvailable] = useState(false)
   
-  useEffect( () => {
-    
-    console.log('here')
-    
+  
+
+  useEffect(() => {
+    const divContainer = document.createElement("divContainer")
     const connect = window.connect;
-    const instanceURL = "https://lonebridge.awsapps.com/connect/ccp#/";  
-  
-    const divContainer = document.createElement("divContainer");
-    console.log(divContainer)
-  
+  const instanceURL = "https://lonebridge.awsapps.com/connect/ccp#/" 
     connect.core.initCCP(divContainer, {
-      ccpUrl: instanceURL,            // REQUIRED
-      loginPopup: false,               // optional, defaults to `true`
-      loginPopupAutoClose: false,      // optional, defaults to `true`
-      loginOptions: {                 // optional, if provided opens login in new window
-        autoClose: false,              // optional, defaults to `false`
-        height: 600,                  // optional, defaults to 578
-        width: 400,                   // optional, defaults to 433
-        top: 0,                       // optional, defaults to 0
-        left: 0                       // optional, defaults to 0
-      },
-      region: "eu-central-1",         // REQUIRED for `CHAT`, optional otherwise
-      softphone: {                    // optional
-        allowFramedSoftphone: true,   // optional
-        disableRingtone: false,       // optional
-        ringtoneUrl: "./ringtone.mp3" // optional
-       }
-     });
-  
-     connect.agent(subscribeToAgentEvents);
+      ccpUrl: instanceURL,
+      loginPopup: true,
+      loginPopupAutoClose: true,
+      region: "us-east-1",
+      softphone: {
+        allowFramedSoftphone: true
+      }
+    });
+  }, []);
 
-  
-  })
-    
 
   function subscribeToAgentEvents(agent) {
     console.log('Inside subscription')
     console.log(agent)
     // Subscribing to Streams API Long Polling Agent events
     window.myCPP.agent = agent;
-   
     
 }
     
@@ -103,30 +83,14 @@ const CallPanel = () => {
   const handleAvailableToggle = () => {
     setAvailable(!available);
   }
-  return (
-    <Card className={classes.root}>
-      <CardHeader
-      
-        avatar={
-          <Avatar aria-label="account" className={classes.avatar}>
-            <AccountCircleIcon fontSize="large" />
-          </Avatar>
-        }
-        action={
-            <FormControlLabel
-          control={<Switch size="small" className={classes.toggleColor} checked={available} onChange={handleAvailableToggle} name="available"  />}
-          label="Available"
-        />
-        }
-        title="Sai Thotapalli"
-        subheader={available ? 'available' : 'offline'}
-      />
-      
-      <CardContent>
-       
-        </CardContent>
 
-    </Card>
-  );
+
+  
+
+return (
+   <div id="divContainer" style={{ width: "320px", height: "500px" }} />
+)
+
+
 }
 export default CallPanel;
