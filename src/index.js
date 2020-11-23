a51f4dc7148c562708e98e7854ee35aa26694d81
SaiThotapalli99 <Sai.Thotapalli@bekognitive.com>
2020-11-23 16:32:28 -0500

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App'
-import 'semantic-ui-css/semantic.min.css'
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import appReducer from './reducers';
